18eaae4b5c0714cf4c99f1f78f0cc8497da72333
jeffreymeatcutter <jeffreymeatcutter@gmail.com>
2025-01-16 13:51:29 -0500

    created API gateway

--- api-gateway-fleet/src/main/java/com/skillstorm/ApiGatewayFleetApplication.java
+++ api-gateway-fleet/src/main/java/com/skillstorm/ApiGatewayFleetApplication.java
@@ -2,14 +2,14 @@ package com.skillstorm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableEurekaServer
+@EnableDiscoveryClient
 @SpringBootApplication
-public class EurekaServerApplication {
+public class ApiGatewayFleetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(ApiGatewayFleetApplication.class, args);
 	}
 
 }
