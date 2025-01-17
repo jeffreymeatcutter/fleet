217bccbe681a2a052adc7a47bb9cfcd86d8db8a4
z6um <laureljohna@gmail.com>
2025-01-17 13:53:17 -0500

    started on personnel service set up

---
+++ personnel-service/src/main/java/com/skillstorm/controllers/PersonnelController.java
@@ -0,0 +1,12 @@
+package com.skillstorm.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/personnel")
+public class PersonnelController {
+	
+	
+
+}
