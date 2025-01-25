4c4656306808d9d8d887b030367409cf68d3177a
jeffreymeatcutter <jeffreymeatcutter@gmail.com>
2025-01-25 13:23:38 -0500

    finished edit squad feature, with error handling

---
+++ frontend/pangolin-app/src/app/components/squad-edit-view/squad-edit-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-squad-edit-view',
+  imports: [],
+  templateUrl: './squad-edit-view.component.html',
+  styleUrl: './squad-edit-view.component.css'
+})
+export class SquadEditViewComponent {
+
+}
--- frontend/pangolin-app/src/app/components/squadron/squadron.component.ts
+++ frontend/pangolin-app/src/app/components/squadron/squadron.component.ts
@@ -2,14 +2,47 @@ import { Component, Input } from '@angular/core';
 import { Squadron } from '../../models/squadron/squadron';
 import { MatCardModule } from '@angular/material/card'
 import { NgIf } from '@angular/common';
+import { SquadronService } from '../../services/squadron.service';
+import { FormsModule } from '@angular/forms';
+import { SquadronDTO } from '../../models/squadron/squadronDTO';
 
 @Component({
   selector: 'app-squadron',
-  imports: [MatCardModule, NgIf],
+  imports: [MatCardModule, NgIf, FormsModule],
   templateUrl: './squadron.component.html',
   styleUrl: './squadron.component.css',
   standalone: true
 })
 export class SquadronComponent {
+  constructor(private squadronService: SquadronService) {}
+
   @Input() squadron: Squadron| null = null;
+  bufferSquadron: Squadron| null = null;
+  editMode: boolean = false;
+
+  toggleEditMode(): void {
+    this.bufferSquadron = {...this.squadron!};
+    this.editMode = !this.editMode;
+    
+    console.log(`${this.bufferSquadron?.maxCapacity}`)
+    console.log(`${this.squadron?.maxCapacity}`)
+  }
+
+  submitChanges(): void {
+    if (this.squadron) {
+      const squadDTO = new SquadronDTO(this.squadron.squadronName, this.squadron.maxCapacity)
+      this.squadronService.update(this.squadron.squadronId, squadDTO).subscribe(
+        response => {
+          this.editMode = false;
+        },
+        error => {
+          alert('Error updating squadron: max capacity too low or 0');
+          this.squadron = this.bufferSquadron;
+        }
+      );
+    }
+  }
+
+
+
 }
