75d5664cfdb0ac45f15093381356616baca9f8b0
jeffreymeatcutter <jeffreymeatcutter@gmail.com>
2025-01-26 13:04:02 -0500

    finished implementing features for personnel for front end

--- frontend/pangolin-app/src/app/components/personnel-view/personnel-view.component.ts
+++ frontend/pangolin-app/src/app/components/personnel-view/personnel-view.component.ts
@@ -3,6 +3,7 @@ import { Personnel } from '../../models/personnel/personnel';
 import { PersonnelService } from '../../services/personnel.service';
 import { CommonModule } from '@angular/common';
 import { PersonnelComponent } from '../personnel/personnel.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-personnel-view',
@@ -14,16 +15,34 @@ import { PersonnelComponent } from '../personnel/personnel.component';
 export class PersonnelViewComponent {
   personnel: Personnel[] = [];
 
-  constructor(private personnelService: PersonnelService) {}
+  constructor(private personnelService: PersonnelService, private router: Router) {}
 
   ngOnInit() {
       //Populate personnel on init
+      this.loadPersonnel();
+    }
+
+    loadPersonnel(): void {
       this.personnelService.findAll().subscribe((data) => {
         this.personnel = data;
       });
     }
+
     // Function to track items by their unique ID
     trackByPersonId(index: number, person: Personnel): number | undefined {
       return person.personnelId;
     }
+    // Handle person deletion
+  handlePersonDeletion(personnelId: number): void {
+    this.personnel = this.personnel.filter(personnel => personnel.personnelId !== personnelId);
+  }
+   // Navigate to the add-squadron route
+   goToAddPersonnel(): void {
+    this.router.navigate(['/add-personnel']);
+  }
+
+
+  handlePersonnelUpdate(updatedPersonnel: Personnel): void {
+    this.loadPersonnel();
+  }
 }
--- frontend/pangolin-app/src/app/components/personnel/personnel.component.ts
+++ frontend/pangolin-app/src/app/components/personnel/personnel.component.ts
@@ -1,26 +1,80 @@
 import { NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Personnel } from '../../models/personnel/personnel';
 import { SquadronService } from '../../services/squadron.service';
+import { FormsModule } from '@angular/forms';
+import { PersonnelService } from '../../services/personnel.service';
+import { PersonnelDTO } from '../../models/personnel/personnelDTO';
 
 @Component({
   selector: 'app-personnel',
-  imports: [MatCardModule, NgIf],
+  imports: [FormsModule, NgIf],
   templateUrl: './personnel.component.html',
   styleUrl: './personnel.component.css',
   standalone: true
 })
 export class PersonnelComponent {
-  constructor(private squadronService: SquadronService) {}
+  constructor(private squadronService: SquadronService, private personnelService: PersonnelService) {}
   @Input() person: Personnel | null = null;
+  @Output() personDeleted = new EventEmitter<number>();
+  @Output() personUpdated = new EventEmitter<Personnel>();
   squadName: string = "";
+  bufferPerson: Personnel | null = null;
+  editMode: boolean = false;
 
   ngOnInit(): void {
     if(this.person){
       this.squadronService.findOne(this.person.squadronId).subscribe((data) => {
         this.squadName = data.squadronName;
       })
+    }
+  }
+
+  toggleEditMode(): void {
+    this.bufferPerson = {...this.person!};
+    this.editMode = !this.editMode;
+  }
+
+  submitChanges(): void {
+    if (this.person) {
+      const personnelDTO = new PersonnelDTO(
+        this.person.personnelName,
+        this.person.commander,
+        this.person.squadronId
+      );
+  
+      this.personnelService.update(this.person.personnelId, personnelDTO).subscribe(
+        (response) => {
+          this.editMode = false;
+          // Create a new instance to avoid mutation issues
+          const updatedPerson = new Personnel(
+            this.person!.personnelId,
+            this.person!.personnelName,
+            this.person!.commander,
+            this.person!.squadronId
+          );
+          this.personUpdated.emit(updatedPerson); // Emit updated personnel
+        },
+        (error) => {
+          alert('Error updating personnel');
+          this.person = this.bufferPerson; // Revert changes on error
+        }
+      );
+    }
+  }
+  
+
+  deletePersonnel(): void {
+    if (this.person) {
+      this.personnelService.delete(this.person.personnelId).subscribe(
+        (response) => {
+          alert('Personnel deleted');
+          this.personDeleted.emit(this.person!.personnelId);
+        },
+        (error) => {
+          alert('Error deleting personnel');
+        }
+      );
     }
   }
 }
