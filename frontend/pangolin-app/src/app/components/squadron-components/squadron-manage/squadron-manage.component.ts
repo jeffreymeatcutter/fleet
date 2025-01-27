ebf98419e5ef8d2af95bbc4067357f4809405594
jeffreymeatcutter <jeffreymeatcutter@gmail.com>
2025-01-27 15:59:21 -0500

    ground work completed for manage squadron

---
+++ frontend/pangolin-app/src/app/components/squadron-components/squadron-manage/squadron-manage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-squadron-manage',
+  imports: [],
+  templateUrl: './squadron-manage.component.html',
+  styleUrl: './squadron-manage.component.css'
+})
+export class SquadronManageComponent {
+
+}
